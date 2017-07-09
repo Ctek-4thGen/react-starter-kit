cac82ae5c8789bd55a3833490e179b2ce89aab7d
Nathan A. Carmona <NAC@Nathans-MacBook-Pro.local>
2017-07-09 18:08:10 -0400

    added decorator functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {AppContainer} from 'react-hot-loader';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  })
+}
+
 registerServiceWorker();
