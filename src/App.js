cc9d0b56dc7cd6e998eff4a102fb0a1dccc1f54e
Nathan A. Carmona <NAC@Nathans-MacBook-Pro.local>
2017-07-09 15:07:52 -0400

    removed state & button

--- src/App.js
+++ src/App.js
@@ -3,33 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-handleClick() {
-  this.setState({number: this.state.number + 1})
-}
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React: {this.state.number}</h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button
-          onClick = {this.handleClick}
-        >
-        click
-        </button>
       </div>
     );
   }
